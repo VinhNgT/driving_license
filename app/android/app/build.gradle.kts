82cd98f3fea4bfacdab25e120240f31598cee4f3
VinhNgT <victorpublic0000@gmail.com>
2025-11-05 14:21:10 +0700

    chore: set app name and bundleId

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.driving_license"
+        applicationId = "com.vikapps.driving_license"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion

4a14fcf251676e60ac259868407d1dd31436ca1e
VinhNgT <victorpublic0000@gmail.com>
2025-11-05 22:13:05 +0700

    refactor: update icon resources

--- scripts/generate_launcher_icon.sh
+++ scripts/generate_launcher_icon.sh
@@ -7,8 +7,8 @@ cd app
 # Please configure flutter_launcher_icons.yaml and flutter_native_splash.yaml
 # before running this script.
 
-echo "Running flutter_launcher_icons..."
-dart run flutter_launcher_icons
-echo
-echo "Running flutter_native_splash..."
-dart run flutter_native_splash:create
+echo "Running icons_launcher:create..."
+dart run icons_launcher:create
+# echo
+# echo "Running flutter_native_splash..."
+# dart run flutter_native_splash:create
